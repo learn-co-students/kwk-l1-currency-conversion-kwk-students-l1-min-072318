4ade918ef0e8077cb4140ca52c24cbec8448a495
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-25 21:25:50 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,48 @@
-# Write your cod here
+def usd_to_eur(usd)
+  puts eur = usd * 0.86
+end
+
+usd_to_eur(1)
+
+def eur_to_usd(eur)
+  puts usd = eur * 1.17
+end
+
+eur_to_usd(1)
+
+def usd_to_jpy
+  puts jpy = usd * 110.96
+end
+
+usd_to_jpy(1)
+
+def jpy_to_usd
+  puts usd = jpy * 0.0090
+end
+
+jpy_to_usd(1)
+  
+
+def usd_to_gbp
+  puts gbp = usd * 0.76
+end
+
+usd_to_gbp(1)
+
+def gbp_to_usd
+  puts usd = gbp * 1.32
+end
+
+gbp_to_usd(1)
+
+def usd_to_aud
+  puts aud = usd * 1.34
+end
+
+usd_to_aud(1)
+
+def aud_to_usd
+  puts usd = aud * 0.75
+end
+  
+aud_to_usd(1)
