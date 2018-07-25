5534f158e465874c5ba3252c9d0f7625f388bae5
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-25 19:17:36 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,56 @@
-# Write your cod here
+def usd_to_eur(dollar_amount)
+  dollar_amount / 1.2
+end 
+
+usd_to_eur(5)
+
+def eur_to_usd (euro_amount)
+  euro_amount * 1.2
+end 
+
+eur_to_usd(7)
+
+def usd_to_jpy (dollar_amount)
+  dollar_amount/0.009
+end 
+
+usd_to_jpy (80)
+
+def usd_to_gbp (dollar_amount)
+  dollar_amount/ 1.32
+end 
+
+usd_to_gbp(5)
+
+def usd_to_aud (dollar_amount)
+  dollar_amount/0.74
+end 
+
+usd_to_aud(10)
+
+def jpy_to_usd (yen_amount)
+  yen_amount * 1 
+end 
+
+jpy_to_usd (7)
+
+def gbp_to_usd (pound_amount)
+  pound_amount * 1 
+end 
+
+gbp_to_usd(18)
+
+def aud_to_usd (austrailian_amount)
+  austrailian_amount * 1 
+end
+
+aud_to_usd(100)
+
+
+
+  
+  
+  
+  
+  
+  
